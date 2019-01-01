d9b23695a0a3230cf96e8cb4d35bbc86c0b28a27
Celso Fernandes <celso.fernandes@gmail.com>
2019-01-02 01:36:33 +0200

    Respect and sitemap pages

--- src/pages/respect.js
+++ src/pages/respect.js
@@ -1,11 +1,146 @@
 import React from "react"
 import withRoot from '../withRoot'
 import MainLayout from '../components/MainLayout'
+import { Link } from 'gatsby'
 
 const RespectPage = () => {
   return (
     <MainLayout>
-      <h1>Respect</h1>
+
+      <h1 id="respect">Respect</h1>
+
+      <p>This website is run solely by one person, <Link to="/about">me</Link>.</p>
+
+      <p>This website is based on mutual respect, what it means? I <strong>don't</strong> use <strong>any</strong> 3rd party services that tracks you, nothing like Google Analytics, Social Media (Facebook, Twitter) widgets that is used to track you.</p>
+
+      <p>If you just navigate on this site, we just collect some technical data to ensure no problem is happening, and to measure the performance.</p>
+
+      <p>If you login to comment on this website, the comments will be stored based solely on the information you provide.</p>
+
+      <p>As I don't use any widgets, if you'd like to share some post/article with a friend, you can use our widget that send via e-mail, and I ensure you, neither you and your friend will be added to any mailing list.</p>
+
+      <p>My mailing list is used solely by communicating purposes with who, actively, told us that want to have a conversation with me, I wanna talk with awesome people that wanna talk back to me. Period.</p>
+
+      <p>How this site makes money? Simple, it doesn't! My job is being a web developer, here is a place where I share information with friends, giving back to the community that always helped me. This site is my _experiment_ over a simple, free web. Where the owner and the users have full control of the medium.</p>
+
+      <p>You can share and modify any work on this website, this is your right, your only duty is to distribute the work with the same licensing described on the <a href="#creative-commons">Creative Commons</a> section, any futher information, just get in <Link to="/contact">contact</Link></p>
+
+      <p>Welcome!</p>
+      
+      <h1 id="legal-statements">Legal Statements</h1>
+
+      Here are some legal statements to be legally clear how we use and manipulate any data.
+
+      <h2 id="creative-commons">Creative Commons</h2>
+
+      <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" style={{borderWidth: 0}} src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a><br /><span xmlnsDct="http://purl.org/dc/terms/" property="dct:title">Coding</span> by <a xmlnsCc="http://creativecommons.org/ns#" href="https://coding.com.br/about" property="cc:attributionName" rel="cc:attributionURL">Celso Vieira Joca Fernandes</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>.<br />Based on a work at <a xmlnsDct="http://purl.org/dc/terms/" href="https://coding.com.br/" rel="dct:source">https://coding.com.br/</a>.<br />Permissions beyond the scope of this license may be available at <a xmlnsCc="http://creativecommons.org/ns#" href="https://coding.com.br/respect" rel="cc:morePermissions">https://coding.com.br/respect</a>.
+
+      <h2 id="privacy-policy">Privacy Policy</h2>
+
+      <p>Effective date: January 01, 2019</p>
+
+      <p>Coding ("I") operate the http://coding.com.br/ website (the "Service").</p>
+
+      <p>This page informs you of our policies regarding the collection, use, and disclosure of personal data when you use my Service and the choices you have associated with that data.</p>
+
+      <p>I use your data to provide and improve the Service. By using the Service, you agree to the collection and use of information in accordance with this policy. Unless otherwise defined in this Privacy Policy, terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, accessible from http://coding.com.br/</p>
+
+      <h3>Information Collection And Use</h3>
+
+      <p>I collect two different types of information for various purposes to provide and improve our Service to you.</p>
+
+      <h4>Types of Data Collected</h4>
+
+      <h5>Personal Data</h5>
+
+      <p>While using my Service, I may ask you to provide us with certain personally identifiable information that can be used to contact or identify you ("Personal Data"). Personally identifiable information may include, but is not limited to:</p>
+
+      <ul>
+        <li>Email address</li>
+        <li>Github Account</li>
+        <li>Cookies and Usage Data</li>
+      </ul>
+
+      <h5>Usage Data</h5>
+
+      <p>I may also collect information how the Service is accessed and used ("Usage Data"). This Usage Data may include information such as your computer's Internet Protocol address (e.g. IP address), browser type, browser version, the pages of our Service that you visit, the time and date of your visit, the time spent on those pages, unique device identifiers and other diagnostic data.</p>
+
+      <h5>Tracking & Cookies Data</h5>
+
+      <p>I use cookies and similar tracking technologies to track the activity on my Service and hold certain information.</p>
+      <p>Cookies are files with small amount of data which may include an anonymous unique identifier. Cookies are sent to your browser from a website and stored on your device. Tracking technologies also used are beacons, tags, and scripts to collect and track information and to improve and analyze our Service.</p>
+      <p>You can instruct your browser to refuse all cookies or to indicate when a cookie is being sent. However, if you do not accept cookies, you may not be able to use some portions of our Service.</p>
+      <p>Examples of Cookies we use:</p>
+
+      <ul>
+          <li><strong>Session Cookies.</strong> I use Session Cookies to operate our Service.</li>
+          <li><strong>Preference Cookies.</strong> I use Preference Cookies to remember your preferences and various settings.</li>
+          <li><strong>Security Cookies.</strong> I use Security Cookies for security purposes.</li>
+      </ul>
+
+      <h3>Use of Data</h3>
+          
+      <p>Coding uses the collected data for various purposes:</p>    
+      <ul>
+          <li>To provide and maintain the Service</li>
+          <li>To notify you about changes to our Service</li>
+          <li>To allow you to participate in interactive features of our Service when you choose to do so</li>
+          <li>To provide customer care and support</li>
+          <li>To provide analysis or valuable information so that I can improve the Service</li>
+          <li>To monitor the usage of the Service</li>
+          <li>To detect, prevent and address technical issues</li>
+      </ul>
+
+      <h3>Transfer Of Data</h3>
+
+      <p>Your information, including Personal Data, may be transferred to — and maintained on — computers located outside of your state, province, country or other governmental jurisdiction where the data protection laws may differ than those from your jurisdiction.</p>
+      <p>If you are located outside United States or Brazil and choose to provide information to us, please note that I transfer the data, including Personal Data, to United States and Brazil and process it there.</p>
+      <p>Your consent to this Privacy Policy followed by your submission of such information represents your agreement to that transfer.</p>
+      <p>Coding will take all steps reasonably necessary to ensure that your data is treated securely and in accordance with this Privacy Policy and no transfer of your Personal Data will take place to an organization or a country unless there are adequate controls in place including the security of your data and other personal information.</p>
+
+      <h3>Disclosure Of Data</h3>
+
+      <h4>Legal Requirements</h4>
+      <p>Coding may disclose your Personal Data in the good faith belief that such action is necessary to:</p>
+      <ul>
+          <li>To comply with a legal obligation</li>
+          <li>To protect and defend the rights or property of Coding</li>
+          <li>To prevent or investigate possible wrongdoing in connection with the Service</li>
+          <li>To protect the personal safety of users of the Service or the public</li>
+          <li>To protect against legal liability</li>
+      </ul>
+
+      <h3>Security Of Data</h3>
+      <p>The security of your data is important to us, but remember that no method of transmission over the Internet, or method of electronic storage is 100% secure. While I strive to use commercially acceptable means to protect your Personal Data, I cannot guarantee its absolute security.</p>
+
+      <h3>Service Providers</h3>
+      <p>I may employ third party companies and individuals to facilitate our Service ("Service Providers"), to provide the Service on our behalf, to perform Service-related services or to assist us in analyzing how our Service is used.</p>
+      <p>These third parties have access to your Personal Data only to perform these tasks on our behalf and are obligated not to disclose or use it for any other purpose.</p>
+      
+      <p>These third parties are limited to</p>
+
+      <ul>
+        <li>Infrastructure (Data Center) Service Provider</li>
+      </ul>
+
+      <h3>Links To Other Sites</h3>
+      <p>My Service may contain links to other sites that are not operated by me. If you click on a third party link, you will be directed to that third party's site. I strongly advise you to review the Privacy Policy of every site you visit.</p>
+      <p>I have no control over and assume no responsibility for the content, privacy policies or practices of any third party sites or services.</p>
+
+      <h3>Children's Privacy</h3>
+      <p>My Service does not address anyone under the age of 18 ("Children").</p>
+      <p>I do not knowingly collect personally identifiable information from anyone under the age of 18. If you are a parent or guardian and you are aware that your Children has provided us with Personal Data, please contact me. If I become aware that we have collected Personal Data from children without verification of parental consent, I take steps to remove that information from our servers.</p>
+
+      <h3>Changes To This Privacy Policy</h3>
+      <p>I may update our Privacy Policy from time to time. I will notify you of any changes by posting the new Privacy Policy on this page.</p>
+      <p>I will let you know via email and/or a prominent notice on our Service, prior to the change becoming effective and update the "effective date" at the top of this Privacy Policy.</p>
+      <p>You are advised to review this Privacy Policy periodically for any changes. Changes to this Privacy Policy are effective when they are posted on this page.</p>
+
+      <h3>Contact Me</h3>
+      <p>If you have any questions about this Privacy Policy, please contact me:</p>
+      <ul>
+        <li>By email: legal at coding.com.br</li>
+      </ul>
     </MainLayout>
   )
 }
--- src/pages/sitemap.js
+++ src/pages/sitemap.js
@@ -1,11 +1,30 @@
 import React from "react"
 import withRoot from '../withRoot'
 import MainLayout from '../components/MainLayout'
+import { Link } from 'gatsby'
 
 const SitemapPage = () => {
   return (
     <MainLayout>
       <h1>Sitemap</h1>
+
+      <p>This is the simplest sitemap ever, just to make sure you enjoy in it's fully potential, here are the links to the most interesting (or not) content.</p>
+
+      <ul>
+        <li><Link to="/">Coding Home</Link> : Here is where everything starts</li>
+        <li><Link to="/articles">Articles</Link> : Some interesting essays organized by category, curated manually (not automatically generated) by me</li>
+        <li><Link to="/talks">Talks</Link> : I love to share some ideas personally with other people, here is where I had the opportunity to</li>
+        <li><Link to="/about">About</Link> : Some random information about me, so you can know who writes here <span role="img" aria-label="Wink">😉</span></li>
+        <li><Link to="/contact">Contact</Link> : Wanna tell me something? Just send a message!</li>
+        <li><Link to="/sitemap">Sitemap</Link> : Probably this page</li>
+        <li><Link to="/respect#respect">Respect</Link> : Here I described, in some legal terms too, how I respect you, your data and what you can expect from this site.</li>
+        <li style={{listStyleType: 'none'}}>
+          <ul>
+            <li><Link to="/respect#creative-commons">Creative Commons</Link> : How the content is licensed</li>
+            <li><Link to="/respect#privacy-policy">Privacy Policy</Link> : Legal statement about privacy policy</li>
+          </ul>
+        </li>
+      </ul>
     </MainLayout>
   )
 }
